0992b29960fb6b57c2bc57b55d1e9f97cd4d92aa
jongtaekim <minahn93@gmail.com>
2019-07-30 17:10:30 +0900

    commitss

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 $(document).ready(() => {
     indexItems();
+
+    const domContainer = document.querySelector('#like_button_container');
+    ReactDOM.render((
+        <button onClick={() => this.setState({ liked: true })}>
+            Like
+        </button>
+    ), domContainer);
 });
 
-function indexItems(){
-    $.get('http://localhost:8004/items/')
+function indexItems() {
+    $.get('http://localhost:8003/items/')
     .done((items) => {
-        for (item of items) {
+        for (const item of items) {
             const $itemContainer = $(`
-    <div class="item-container" 
-    onClick="location.href = '/item_detail.html?id=${item.id}'">
+    <div class="item-container"
+    onClick="location.href = '/item-detail.html?id=${item.id}'">
         <img src="${item.image}" alt="">
-            <p class="item-title">${item.title}</p>
-            <p class="item-price">가격 : ${item.price}</p>
+        <p class="item-title">${item.title}</p>
+        <p class="item-price">가격 : ${item.price}</p>
     </div>`);
             const itemListContainer = $('#item-list-container');
             $itemContainer.appendTo(itemListContainer);
--- item_detail.js
+++ item_detail.js
@@ -3,30 +3,28 @@ let itemId = 0;
 $(document).ready(() => {
     const url = new URL(location.href);
     itemId = url.searchParams.get("id");
-    getItem(itemId)
+    getItem(itemId);
 });
 
-function getItem(itemId){
-    $.get('http://localhost:8004/items/' + itemId + '/')
-    .done((item) => {
-        $('.item-image-container > img').attr('src', item.image);
-        $('.item-detail-container > p > b').text('상품명 : ' + item.title);
-        const descElement = $('.item-detail-container > p')[1];
-        $(descElement).text('설명 : ' + item.description);
-    });
+function getItem(itemId) {
+    $.get('http://localhost:8003/items/' + itemId + '/')
+        .done((item) => {
+            $('.item-image-container > img').attr('src', item.image);
+            $('.item-detail-container > p > b').text('상품명 : ' + item.title);
+            const descElement = $('.item-detail-container > p')[1];
+            $(descElement).text('설명 : ' + item.description);
+        });
 }
 
 function purchase() {
     $.ajax({
-        url: 'http://localhost:8004/items/' + itemId + '/purchase/',
+        url: 'http://localhost:8003/items/' + itemId + '/purchase/',
         type: 'post',
         dataType: 'json',
         beforeSend: function (xhr) {
-            const username = 'admin';
-            const password = 'admin';
-            xhr.setRequestHeader("Authorization", "Basic " + btoa(username + ":" + password));
+            xhr.setRequestHeader("Authorization", localStorage.getItem('authorization'));
         },
     }).done((result) => {
-        location.href = '/my_items.html';
+        location.href = '/my-items.html';
     });
 }
